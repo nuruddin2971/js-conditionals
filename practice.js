4c18a476923c97aa4b8b2c74bfc0b4f97b112491
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-16 23:32:52 +0600

    task-2

--- practice.js
+++ practice.js
@@ -1,13 +1,17 @@
 // Question-1
-const price1 = 5;
-const price2 = 7;
+// const price1 = 5;
+// const price2 = 7;
 
-console.log(price1 == price2);
-console.log(price1 === price2);
-console.log(price1 !== price2);
-console.log(price1 > price2);
-console.log(price1 < price2);
-console.log(price1 >= price2);
-console.log(price1 <= price2);
+// console.log(price1 == price2);
+// console.log(price1 === price2);
+// console.log(price1 !== price2);
+// console.log(price1 > price2);
+// console.log(price1 < price2);
+// console.log(price1 >= price2);
+// console.log(price1 <= price2);
 
-
+// Question-2
+const result1 = "5";
+const result2 = 5;
+console.log(result1 == result2);
+console.log(result1 === result2);
