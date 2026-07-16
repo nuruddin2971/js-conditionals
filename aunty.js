98be60fbaed61c2ed869181350d31333e2bb6d2f
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-16 11:23:39 +0600

    first commit

---
+++ aunty.js
@@ -0,0 +1,9 @@
+const a = 10;
+const b = 7;
+
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a === b);
+// console.log(a >= b);
+// console.log(a <= b);
+console.log(a !== b);
---
+++ if.js
@@ -0,0 +1,11 @@
+// if (true) {
+//   console.log("inside the condition");
+// }
+// if (false) {
+//   console.log("inside the condition");
+// }
+
+const price = 1400;
+if (price < 500) {
+  console.log("price is lower");
+}
