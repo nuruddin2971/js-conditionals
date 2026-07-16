ba8f7d6dd541c47897c3f3fe941e58d7350783d5
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-17 00:04:11 +0600

    complete task 9

--- practice.js
+++ practice.js
@@ -11,7 +11,57 @@
 // console.log(price1 <= price2);
 
 // Question-2
-const result1 = "5";
-const result2 = 5;
-console.log(result1 == result2);
-console.log(result1 === result2);
+// const result1 = "5";
+// const result2 = 5;
+// console.log(result1 == result2);
+// console.log(result1 === result2);
+
+// Question-3
+// const isRaining = true;
+// if (isRaining) {
+//   console.log("Take an umbrella");
+// }
+
+// Question-4
+// const stock = 0;
+// if (stock < 1) {
+//   console.log("Out of stock");
+// }
+
+// Question-5
+// const number = 5;
+// if (number > 0) {
+//   console.log("It is positive number");
+// } else {
+//   console.log("It is negative number");
+// }
+
+// Question-6
+// let year = 2028;
+// if (year % 4 === 0) {
+//     console.log('It is a leap year')
+// }
+
+// Question-7
+// const speed = 90;
+// if (speed > 80) {
+//   console.log("Overspeeding");
+// } else {
+//   console.log("Normal speed");
+// }
+
+// Question-8
+// const age = 19;
+// const hasTicket = true;
+// if (age > 18 && hasTicket) {
+//   console.log("Entry allowed");
+// } else {
+//   console.log("No entry");
+// }
+
+// Question-9
+// const isWeekend = true;
+// const isHoliday = true;
+// if (isWeekend || isHoliday) {
+//   console.log("No work today");
+// }
