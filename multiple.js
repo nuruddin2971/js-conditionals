6e12f93fea56a9a4bc03f2d2c785ab3862944da2
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-16 12:45:02 +0600

    multiple conditions

---
+++ multiple.js
@@ -0,0 +1,17 @@
+// Multiple conditions
+
+const slary = 25000;
+const isBCS = true;
+const hasCar = true;
+
+if (slary > 50000 && isBCS === true && hasCar) {
+  //   console.log("su..........patro");
+} else {
+  //   console.log("Tor kopale ............");
+}
+
+if (slary > 75000 || isBCS === true) {
+  console.log("bolo baba kobul");
+} else {
+  console.log("Vaag tui mokbul");
+}
