1464c4215acb61b52778f46f9c81bc980aa980ec
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-16 15:03:21 +0600

    nested system

---
+++ nested.js
@@ -0,0 +1,17 @@
+const money = 800;
+
+if (money > 500) {
+  console.log("mama tui to borolok");
+
+  if (money > 1000) {
+    console.log("tui ki tuition er salary paicos");
+
+    if (money > 5000) {
+      console.log("tui to amar apon vai");
+    } else {
+      console.log("you have between 1000 - 5000");
+    }
+  }
+} else {
+  console.log("amar kono borolok bondhu nai");
+}
