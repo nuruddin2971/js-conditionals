45265f15f8dd98cff096ce8ae761d272f70d6e8f
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-16 12:24:48 +0600

    else condition

---
+++ else.js
@@ -0,0 +1,9 @@
+const rickshawFare = 200;
+
+if (rickshawFare < 50) {
+  console.log("mama hood tule den");
+  console.log("go slowly");
+} else {
+  console.log("walking is also exercising");
+  console.log("I am a situational runner");
+}
