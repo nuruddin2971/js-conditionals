77a954f31eee31005018bf251e2ecec7ec99ed26
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-16 14:43:20 +0600

    grade system

---
+++ grade.js
@@ -0,0 +1,11 @@
+const marks = 55;
+
+if (marks > 80) {
+  console.log("A+");
+} else if (marks > 70) {
+  console.log("B+");
+} else if (marks > 60) {
+  console.log("D+");
+} else {
+  console.log("F");
+}
