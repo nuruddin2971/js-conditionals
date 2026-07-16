3a5de59cedc1e94443bc954c46e15e4854c1b782
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-16 18:22:06 +0600

    logical not operator

---
+++ not.js
@@ -0,0 +1,15 @@
+const isLoggedIn = false;
+
+if(!isLoggedIn){
+    console.log('Please login')
+}
+
+// if (isLoggedIn === false) {
+//   console.log("Please login");
+// }
+
+// if (isLoggedIn === true) {
+//   console.log("add your comment");
+// } else {
+//   console.log("please login");
+// }
